fb84cfeeabe45955fdbe45606b06c1026cad6e36
manalicreations <filoso123987@gmail.com>
2020-01-04 23:44:06 +0530

    new

---
+++ public/workspace/assets/js/main.js
@@ -0,0 +1,69 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBK3oGh_uw-7tpOCR8YsRtEMWo9BsaTddM",
+    authDomain: "gssywebsite.firebaseapp.com",
+    databaseURL: "https://gssywebsite.firebaseio.com",
+    projectId: "gssywebsite",
+    storageBucket: "gssywebsite.appspot.com",
+    messagingSenderId: "894894241763",
+    appId: "1:894894241763:web:285feb81a66dd4a7f712d6",
+    measurementId: "G-48GLDYJ2P1"
+};
+// Initialize Firebase
+//  import * as firebase from 'firebase/app';
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e) {
+    e.preventDefault();
+
+    // Get values
+    var f = getInputVal('f');
+    var l = getInputVal('l');
+    var cy = getInputVal('cy');
+    var City = getInputVal('City');
+    var e = getInputVal('e');
+    var m = getInputVal('m');
+    var message = getInputVal('message');
+
+    // Save message
+    saveMessage(f, l, cy, City, e, m, message);
+
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+
+    // Hide alert after 3 seconds
+    setTimeout(function () {
+        document.querySelector('.alert').style.display = 'none';
+    }, 3000);
+
+    // Clear form
+    // document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id) {
+    return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(f, l, cy, City, e, m, message) {
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        f: f,
+        l: l,
+        cy: cy,
+        City: City,
+        e: e,
+        m: m,
+        message: message
+    });
+}
